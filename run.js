8ef70601b738623b272845e6c508a825d0ffca0d
Andriy Hurskiy <andrewhurskiy@gmail.com>
2021-04-16 18:12:18 +0300

    Added expected output

--- run.js
+++ run.js
@@ -12,164 +12,159 @@
 // 6) Elevator should stop elevating if there are no more passengers inside
 
 class Person {
-	name
-	weight
-	targetFloor
-	needReminder
+    
+    constructor(
+        name='John',
+        weight=75,
+        targetFloor=15,
+        needReminder=false
+    ) {
+        this.name = name
+        this.weight = weight
+        this.targetFloor = targetFloor
+        this.needReminder = needReminder
+    }
 
-	constructor(
-		name='John',
-		weight=75,
-		targetFloor=15,
-		needReminder=false
-		) {
-		this.name = name
-		this.weight = weight
-		this.targetFloor = targetFloor
-		this.needReminder = needReminder
-	}
+    enterElevator(){
+        console.log(`My name: ${this.name}  and weight: ${this.weight} and target floor is ${this.targetFloor} floor.`)
+    }
 
-	enterElevator(){
-		console.log(`My name: ${this.name}  and weight: ${this.weight} and target floor is ${this.targetFloor} floor.`)
-	}
-
-	leaveElevator(currentFloor){
-		console.log(`My name is ${this.name} and my weight is ${this.weight} and I am leaving elevator on ${currentFloor}-th floor.`)
-	}
+    leaveElevator(currentFloor){
+        console.log(`My name is ${this.name} and my weight is ${this.weight} and I am leaving elevator on ${currentFloor}-th floor.`)
+    }
 }
 class Elevator {
 
-	INITIAL_FLOOR = 2
-	AMOUNT_OF_FLOORS = 30
-	MAX_AMOUNT_OF_PASSENGERS = 5
-	MAX_TOTAL_WEIGHT = 400 
+    constructor(){
+        this.INITIAL_FLOOR = 2
+        this.AMOUNT_OF_FLOORS = 30
+        this.MAX_AMOUNT_OF_PASSENGERS = 5
+        this.MAX_TOTAL_WEIGHT = 400 
+	
+        this.currentAmountOfPassengers = 0
+        this.currentWeight = 0
+	
+        this.floorsPassengers = new Map()
+        this.notifiersMap = new Map()
+    }
 
-	currentAmountOfPassengers = 0
-	currentWeight = 0
-	currentAmountOfPassengers = 0
-
-	floorsPassengers = new Map()
-	notifiersMap = new Map()
-
+    elevate(passengers){
+        this.registerPassengers(passengers)
+        this.deliverPassengers()
+        this.resetState()
+    }
 
 
-	elevate(passengers){
-		this.registerPassengers(passengers)
-		this.deliverPassengers()
-		this.resetState()
-	}
+    registerPassengers(listOfPassengers){
+        this.resetFloorsMap()
 
-
-	registerPassengers(listOfPassengers){
-		this.resetFloorsMap()
-
-		while (
-			this.isThereStillPlaceForAnybody() && 
+        while (
+            this.isThereStillPlaceForAnybody() && 
 			this.isThereAnybodyInside(listOfPassengers)
-			){
-				this.register(listOfPassengers.shift())
-		}
-		console.log(`Passengers are registered, total amount of them: ${this.currentAmountOfPassengers} and total weight: ${this.currentWeight}.`)
-	}
+        ){
+            this.register(listOfPassengers.shift())
+        }
+        console.log(`Passengers are registered, total amount of them: ${this.currentAmountOfPassengers} and total weight: ${this.currentWeight}.`)
+    }
 
-	resetFloorsMap(){
-		for (
-			let floorIndex = this.INITIAL_FLOOR;
-			floorIndex <= this.AMOUNT_OF_FLOORS;
-			floorIndex++)
-			{
-			this.floorsPassengers.set(floorIndex, new Array())
-		}
-	}
+    resetFloorsMap(){
+        for (
+            let floorIndex = this.INITIAL_FLOOR;
+            floorIndex <= this.AMOUNT_OF_FLOORS;
+            floorIndex++)
+        {
+            this.floorsPassengers.set(floorIndex, new Array())
+        }
+    }
 
-	isThereStillPlaceForAnybody(){
-		return this.currentAmountOfPassengers < this.MAX_AMOUNT_OF_PASSENGERS
-	}
+    isThereStillPlaceForAnybody(){
+        return this.currentAmountOfPassengers < this.MAX_AMOUNT_OF_PASSENGERS
+    }
 
-	isThereAnybodyInside(passengers){
-		return passengers.length !== 0
-	}
+    isThereAnybodyInside(passengers){
+        return passengers.length !== 0
+    }
 
-	register(passengerToRegister){
-		if (this.isMaxWeightNoExceeded(passengerToRegister)){
-			this.assignPassengerToFloor(passengerToRegister)
-			this.currentAmountOfPassengers++
-			this.currentWeight += passengerToRegister.weight
-			if (passengerToRegister.needReminder === true){
-				this.addPassengerToNotifierList(passengerToRegister)
-			}
-		}
-	}
+    register(passengerToRegister){
+        if (this.isMaxWeightNoExceeded(passengerToRegister)){
+            this.assignPassengerToFloor(passengerToRegister)
+            this.currentAmountOfPassengers++
+            this.currentWeight += passengerToRegister.weight
+            if (passengerToRegister.needReminder === true){
+                this.addPassengerToNotifierList(passengerToRegister)
+            }
+        }
+    }
 
-	isMaxWeightNoExceeded(potentialPassenger){
-		return (this.currentWeight + potentialPassenger.weight) < this.MAX_TOTAL_WEIGHT
-	}
+    isMaxWeightNoExceeded(potentialPassenger){
+        return (this.currentWeight + potentialPassenger.weight) < this.MAX_TOTAL_WEIGHT
+    }
 
-	assignPassengerToFloor(passengerToAdd){
-		this.floorsPassengers
-			.get(passengerToAdd.targetFloor)
-			.push(passengerToAdd)
-	}
+    assignPassengerToFloor(passengerToAdd){
+        this.floorsPassengers
+            .get(passengerToAdd.targetFloor)
+            .push(passengerToAdd)
+    }
 
-	addPassengerToNotifierList(passengerThatNeedNotification){
-		this.notifiersMap.set(
-			passengerThatNeedNotification.targetFloor,
-			passengerThatNeedNotification.name)
-	}
+    addPassengerToNotifierList(passengerThatNeedNotification){
+        this.notifiersMap.set(
+            passengerThatNeedNotification.targetFloor,
+            passengerThatNeedNotification.name)
+    }
 
 
-	deliverPassengers(){
+    deliverPassengers(){
 
-		for (
-			let floorIndex = this.INITIAL_FLOOR;
-			floorIndex <= this.AMOUNT_OF_FLOORS;
-			floorIndex++)
-			{
-			if (this.isThereAnyPassengerInElevator()){
-				if (this.isAnyBodyGoesOut(floorIndex)){
-					console.log(`We are stopping at the floor number ${floorIndex}, since there are passengers to go out.`)
-					this.passengersGoOut(floorIndex)
-				}
-			}else {
-				break
-			}
-			console.log(`We are passing floor ${floorIndex} floor.`)
-		}
-	}
+        for (
+            let floorIndex = this.INITIAL_FLOOR;
+            floorIndex <= this.AMOUNT_OF_FLOORS;
+            floorIndex++)
+        {
+            if (this.isThereAnyPassengerInElevator()){
+                if (this.isAnyBodyGoesOut(floorIndex)){
+                    console.log(`We are stopping at the floor number ${floorIndex}, since there are passengers to go out.`)
+                    this.passengersGoOut(floorIndex)
+                }
+            }else {
+                break
+            }
+            console.log(`We are passing floor ${floorIndex} floor.`)
+        }
+    }
 
-	isThereAnyPassengerInElevator(){
-		return (this.currentAmountOfPassengers > 0)
-	}
-	isAnyBodyGoesOut(floor){
-		return (Object.keys(this.floorsPassengers.get(floor)).length !== 0)
-	}
+    isThereAnyPassengerInElevator(){
+        return (this.currentAmountOfPassengers > 0)
+    }
+    isAnyBodyGoesOut(floor){
+        return (Object.keys(this.floorsPassengers.get(floor)).length !== 0)
+    }
 
-	passengersGoOut(floor){
-		let passengersForThisFloor = this.floorsPassengers.get(floor)
-		this.floorsPassengers.delete(floor)
+    passengersGoOut(floor){
+        let passengersForThisFloor = this.floorsPassengers.get(floor)
+        this.floorsPassengers.delete(floor)
 
-		this.currentAmountOfPassengers = this.currentAmountOfPassengers - passengersForThisFloor.length
+        this.currentAmountOfPassengers = this.currentAmountOfPassengers - passengersForThisFloor.length
 		
-		for (let passenger of passengersForThisFloor){
-			this.remindPassengerIfNecessary(passenger)
-			passenger.leaveElevator(floor)
-		}
-	}
+        for (let passenger of passengersForThisFloor){
+            this.remindPassengerIfNecessary(passenger)
+            passenger.leaveElevator(floor)
+        }
+    }
 
-	remindPassengerIfNecessary(passenger){
-		if (passenger.needReminder){
-			console.log(`Reminder for  ${passenger.name}. Please leave.`)
-		}
-	}
+    remindPassengerIfNecessary(passenger){
+        if (passenger.needReminder){
+            console.log(`Reminder for  ${passenger.name}. Please leave.`)
+        }
+    }
 
 
-	resetState(){
-		this.currentAmountOfPassengers = 0
-		this.currentWeight = 0
-		this.currentAmountOfPassengers = 0
-		this.floorsPassengers = new Map()
-		this.notifiersMap = new Map()
-	}
+    resetState(){
+        this.currentAmountOfPassengers = 0
+        this.currentWeight = 0
+        this.currentAmountOfPassengers = 0
+        this.floorsPassengers = new Map()
+        this.notifiersMap = new Map()
+    }
 
 }
 
@@ -197,24 +192,24 @@ let tom = new Person('Tom', 76, 2, true)
 let jim = new Person('Jim', 56, 29, false)
 let july = new Person('July', 65, 30, true)
 
-const normalPassengers = [
-	ivan, vika, sveta, nastya, vov4ik
+let normalPassengers = [
+    ivan, vika, sveta, nastya, vov4ik
 ]
 
-const bigPassengers = [
-	sveta, max, ivan
+let bigPassengers = [
+    sveta, max, ivan
 ]
 
-const passengersThatNeedReminders = [
-	petia, vasya, ivan, max, vlad, vika
+let passengersThatNeedReminders = [
+    petia, vasya, ivan, max, vlad, vika
 ]
 
-const edgeFloorsPassengers = [
-	bob, tom, jim, july
+let edgeFloorsPassengers = [
+    bob, tom, jim, july
 ]
 
 let tooManyPassengers = [
-	ivan, vika, bob, tom, july, vasya, vlad, vika
+    ivan, vika, bob, tom, july, vasya, vlad, vika, alex
 ]
 
 
